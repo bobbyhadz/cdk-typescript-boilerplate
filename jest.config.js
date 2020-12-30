cd5c13fcf219155203f63f57dab6f07f81d7dccb
Borislav Hadzhiev <opmftw@gmail.com>
2020-12-30 19:29:21 +0200

    updated added rimraf for cdk.out directory cleanup and added integrate.yaml for github actions

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,7 @@
 module.exports = {
-  roots: ['<rootDir>/test'],
+  roots: ['<rootDir>/src', '<rootDir>/infra'],
   testMatch: ['**/*.test.ts'],
   transform: {
-    '^.+\\.tsx?$': 'ts-jest'
-  }
+    '^.+\\.tsx?$': 'ts-jest',
+  },
 };
